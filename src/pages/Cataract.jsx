af72b6885b9c815de438df98087d184abc66454b
ch4mi2 <chamithu4@gmail.com>
2023-08-30 01:28:02 +0530

    feat: cataract route,page,component

---
+++ src/pages/Cataract.jsx
@@ -0,0 +1,12 @@
+import CataractUpload from '../components/Cataract/CataractUpload';
+
+const Cataract = () => {
+  return (
+    <>
+      <h1>Cataract Page</h1>
+      <CataractUpload />
+    </>
+  );
+};
+
+export default Cataract;
