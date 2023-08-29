39753de29118cf8135bd6c7cf171e68dd4257c6a
Navojith <navojith22@gmail.com>
2023-08-29 20:30:15 +0530

    fix: tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Footer from './components/common/Footer';
+import Header from './components/common/Header';
 import AppRoutes from './routes/app-routes';
 
 function App() {
   return (
     <div>
+      <Header />
       <AppRoutes />
+      <Footer />
     </div>
   );
 }
