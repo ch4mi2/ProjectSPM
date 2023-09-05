65105f8634e9001aba21a1b5910cd9e5c6bbed33
Navojith <navojith22@gmail.com>
2023-09-05 21:05:24 +0530

    feat: glaucoma backend

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,7 +1,8 @@
 # app/routes.py
 from flask import request, jsonify
 from app import app
-from app.controllers import cataract_controller  
+from app.controllers import cataract_controller
+from app.controllers import glaucoma_controller  
 
 @app.route('/')
 def index():
@@ -11,3 +12,7 @@ def index():
 @app.route('/predict/cataract', methods=['POST'])
 def predict_cataract():
     return cataract_controller.predict_cataract(request)
+
+@app.route('/predict/glaucoma', methods=['POST'])
+def predict_glaucoma():
+    return glaucoma_controller.predict_glaucoma(request)
