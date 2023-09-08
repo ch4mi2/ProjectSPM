aa5cb4a6cf947c34cf5898055121d07c2bfaec1b
Navojith <navojith22@gmail.com>
2023-09-08 13:43:03 +0530

    feat: glaucoma frontend

--- client/src/pages/Glaucoma.jsx
+++ client/src/pages/Glaucoma.jsx
@@ -2,9 +2,9 @@ import GlaucomaUpload from '../components/Glaucoma/GlaucomaUpload';
 
 const Glaucoma = () => {
   return (
-    <>
+    <div className="flex justify-center">
       <GlaucomaUpload />
-    </>
+    </div>
   );
 };
 
