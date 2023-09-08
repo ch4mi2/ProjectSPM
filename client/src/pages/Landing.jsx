0b2af0c7d4cabe6d4601e56028148cbbce27cf21
Navojith <navojith22@gmail.com>
2023-09-08 17:05:08 +0530

    feat: members

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,9 +1,12 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Member from '../components/Members';
+import Pawara from '/assets/images/pawara.jpg';
 
 const Landing = () => {
   return (
     <div className="bg-white">
+      {console.log(Pawara)}
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
@@ -13,7 +16,7 @@ const Landing = () => {
             className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ffa726] to-[#d32f2f] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
             }}
           />
         </div>
@@ -37,6 +40,34 @@ const Landing = () => {
             </div>
           </div>
         </div>
+        <div className="my-16">
+          <center>
+            <h1 className=" text-xl font-bold tracking-tight text-gray-900 ">
+              Team Members
+            </h1>
+            <div className="ml-[-1rem] flex flex-row flex-wrap place-content-evenly space-evenly w-4/5">
+              <Member
+                github=""
+                img={''}
+                linkedIn=""
+                name={'Dinal Senadheera'}
+              />
+              <Member
+                github="https://github.com/Navojith"
+                img={Pawara}
+                linkedIn="https://www.linkedin.com/in/pawara-navojith-aa607a208/"
+                name={'Pawara Navojith'}
+              />
+              <Member
+                github=""
+                img={''}
+                linkedIn=""
+                name={'Chamithu Gunawardhana'}
+              />
+              <Member github="" img={''} linkedIn="" name={'Shenan Perera'} />
+            </div>
+          </center>
+        </div>
         <div
           className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
           aria-hidden="true"
@@ -45,7 +76,7 @@ const Landing = () => {
             className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
             }}
           />
         </div>
