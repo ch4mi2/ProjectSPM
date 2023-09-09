b39003a79a1d1da844a7082327105db59cd226c3
ShenanPerera <shenaperera2001@gmail.com>
2023-09-09 13:04:03 +0530

    Upload
    Signed-off-by: ShenanPerera <shenaperera2001@gmail.com>

--- client/src/components/Gallery/upload/ImageForm.jsx
+++ client/src/components/Gallery/upload/ImageForm.jsx
@@ -1,14 +1,27 @@
-import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ProgressBar from "../ProgressItem";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../../../firebase/config"
+import uploadFileProgress from "../../../firebase/uploadFIleProgress";
 
 const ImageForm = () => {
     const [file,setFile] = useState(null);
     const [error,setError] = useState(null);
+    const [currentUser , setCurrentUser] = useState(null);
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setCurrentUser(user)
+                console.log(user.email);
+            } 
+        });
+    },[])
+    
 
     const types = ['image/png','image/jpeg'];
 
-    const handleChange = (e) => {
+    const handleChange = async(e) => {
         let selected = e.target.files[0];
         
         if(selected && types.includes(selected.type)){
@@ -19,6 +32,22 @@ const ImageForm = () => {
             setFile(null);
             setError('Please select a image file (png/jpeg)');
         }
+
+            const imageName = new Date().getTime() + selected.name;
+            //  uuidv4() + "."+ file.name.split('.').pop()
+            try{
+                const url = await uploadFileProgress(
+                    selected,
+                    `gallery/${currentUser.uid}`,
+                    imageName,
+                    // setProgress
+                )
+                console.log(url)
+                // setImageURL(null)a
+            }catch(error){
+                alert(error.message)
+                console.log(error)
+            }
     } 
     
 
@@ -29,7 +58,7 @@ const ImageForm = () => {
         <div className="output">
                 {error && <div className="error">{error}</div>}
                 {file && <div>{file.name}</div>}
-                {file && <ProgressBar file={file} setFile={setFile}/>} 
+                {/* {file && <ProgressBar file={file} user={currentUser}/>}  */}
         </div>
         </form>
     )
--- client/src/firebase/uploadFIleProgress.jsx
+++ client/src/firebase/uploadFIleProgress.jsx
@@ -1,20 +1,19 @@
 import { ref , getDownloadURL, uploadBytesResumable } from "firebase/storage";
 import {storage} from "./config";
 
-const uploadFileProgress = (file , subFolder , imageName , setProgress) => {
+const uploadFileProgress = (file , subFolder , imageName ) => {
     return new Promise((resolve , reject)=>{
         const storageRef = ref(storage, subFolder + '/' + imageName)
         const upload = uploadBytesResumable(storageRef, file)
         upload.on('state_change',(snapshot) => {
             const progress = (snapshot.bytesTransferred/ snapshot.totalBytes)*100
             console.log(progress);
-            setProgress(progress)
+            // setProgress(progress)
         } , (error)=>{
             reject(error)
         } ,async() => {
             try{
                 const url = await getDownloadURL(storageRef)
-                console.log('url');
                 resolve(url)
             }catch (error){
                 reject(error)
