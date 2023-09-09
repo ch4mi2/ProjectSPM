b2ae16991b4cb33f5a5b8c2baf5d58c68e9730bf
Dinal <dinalsenadheera@gmail.com>
2023-09-09 20:16:20 +0530

    fix: clasname error

--- client/src/components/common/Footer.jsx
+++ client/src/components/common/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <footer class="bg-gray-900">
-      <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Retina™</a>. All Rights Reserved.</span>
+    <footer className="bg-gray-900">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Retina™</a>. All Rights Reserved.</span>
       </div>
     </footer>
   );
